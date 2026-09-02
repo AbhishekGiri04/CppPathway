93074e4b58b9c9f03494eaff1200fe56b44decb7
AbhishekGiri04 <abhishekgiri0405@gmail.com>
2026-09-02 21:10:46 +0530

    Add Docs

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<iostream> // Preprocessor directive to include the input-output stream library
+using namespace std; // Standard namespace declaration
+
+int main(){ // Main function - execution starts here
+    cout<<"Hello World"<<endl; // Print "Hello World" followed by a newline
+    return 0; // Return 0 to indicate successful execution
+} 
+// End of main.cpp
